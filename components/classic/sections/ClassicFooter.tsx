c6fd1963e1751af9759316401a902119cdb557c2
amr mousa <amrmousa1682@gmail.com>
2025-11-13 01:37:29 +0200

    refactor: simplify ClassicFooter component by removing unused props and sections

--- components/classic/sections/ClassicFooter.tsx
+++ components/classic/sections/ClassicFooter.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import styles from "../ClassicMode.module.css";
 import { QuickLink, SocialLink } from "../types";
 
@@ -13,51 +12,11 @@ interface ClassicFooterProps {
 }
 
 export async function ClassicFooter({
-  description,
-  quickLinksTitle,
-  quickLinksItems,
-  socialTitle,
-  socialItems,
   copyright,
   madeWith,
 }: ClassicFooterProps) {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footerContent}>
-        <div className={styles.footerSection}>
-          <p className={styles.footerDescription}>{description}</p>
-        </div>
-
-        <div className={styles.footerSection}>
-          <h3 className={styles.footerTitle}>{quickLinksTitle}</h3>
-          <div className={styles.footerLinks}>
-            {quickLinksItems.map((link: QuickLink, idx: number) => (
-              <Link key={idx} href={link.href} className={styles.footerLink}>
-                {link.label}
-              </Link>
-            ))}
-          </div>
-        </div>
-
-        <div className={styles.footerSection}>
-          <h3 className={styles.footerTitle}>{socialTitle}</h3>
-          <div className={styles.footerSocial}>
-            {socialItems.map((social: SocialLink, idx: number) => (
-              <a
-                key={idx}
-                href={social.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={styles.footerSocialLink}
-                title={social.label}
-              >
-                {social.label}
-              </a>
-            ))}
-          </div>
-        </div>
-      </div>
-
       <div className={styles.footerBottom}>
         <p className={styles.copyright}>{copyright}</p>
         <p className={styles.madeWith}>{madeWith}</p>
